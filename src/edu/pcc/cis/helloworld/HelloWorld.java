6bb170ca4001e1e495545005bcfbe098233960d7
Xsnumas <acranston90@gmail.com>
2023-02-05 13:20:30 -0800

    Added date to the print command.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("This was written by Alan Cranston for CIS 233J at PCC.");
         System.out.println("My username in GitHub is Xsnumas.");
         System.out.println("I'm not entirely sure how to change it.");
+        System.out.println(new Date());
     }
 }
